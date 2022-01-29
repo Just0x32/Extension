7c1a7349f9a36661c9b31c537a6a83f0f6a90924
Just <jalui@mail.ru>
2022-01-29 05:32:15 +0300

    Reflection for instances added.

--- ReflectionExtension/Reflection.cs
+++ ReflectionExtension/Reflection.cs
@@ -5,27 +5,27 @@ namespace ReflectionExtension
 {
     public static class Reflection
     {
-        public static void SetFieldValue(Type type, string fieldName, object value)
+        public static void SetFieldValue(Type type, string fieldName, object value, object typeInstance = null)
         {
-            FieldInfo field = type.GetField(fieldName, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
-            field.SetValue(new object(), value);
+            FieldInfo field = type.GetField(fieldName, BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            field.SetValue(typeInstance, value);
         }
 
-        public static object GetFieldValue(Type type, string fieldName)
+        public static object GetFieldValue(Type type, string fieldName, object typeInstance = null)
         {
-            FieldInfo field = type.GetField(fieldName, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
-            return field.GetValue(new object());
+            FieldInfo field = type.GetField(fieldName, BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            return field.GetValue(typeInstance);
         }
 
-        public static void SetPropertyValue(Type type, string propertyName, object value)
+        public static void SetPropertyValue(Type type, string propertyName, object value, object typeInstance = null)
         {
-            PropertyInfo property = type.GetProperty(propertyName, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
-            property.SetValue(new object(), value);
+            PropertyInfo property = type.GetProperty(propertyName, BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            property.SetValue(typeInstance, value);
         }
 
-        public static object GetMethodResult(Type type, string methodName, params object[] parameters)
+        public static object GetMethodResult(Type type, string methodName, object typeInstance, params object[] parameters)
         {
-            return type.InvokeMember(methodName, BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public, null, new object(), parameters);
+            return type.InvokeMember(methodName, BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public, null, typeInstance, parameters);
         }
     }
 }
